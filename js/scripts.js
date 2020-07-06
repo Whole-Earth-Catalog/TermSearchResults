a67b5c2cf2f7f696c6d071d4cf1b565c528e3b2b
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-07-05 20:21:18 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log("Beginning js script 7:59")
 var term_keys = []
-$.getJSON("/data/keys_by_decade.json", function (data) {
+$.getJSON("../TermSearchResults/data/keys_by_decade.json", function (data) {
     console("in json")
     $.each(data, function (d) {
         console.log(d.term_key)
@@ -44,17 +44,99 @@ var svg = d3
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Define dataset
-d3.json("/data/keys_by_decade.json", function (data) {
+d3.json("../TermSearchResults/data/keys_by_decade.json", function (data) {
     console.log("reading from json...")
     console.log(data)
     // set color domain
-   
+    color.domain(term_keys)
     // format data
     data.forEach(function (d) {
         d.decade = parseYear(d.decade)
         d.num_ids = +num_ids
     });
-    
+    var key_data = term_keys.map(function (term) {
+        return {
+            term_key: term,
+            datapoints: data.map(function (d) {
+                return { decade: d.decade, count: d.num_ids };
+            })
+        }
+    })
+    console.log(key_data)
+    // set domain of axes
+    xScale.domain(d3.extent(data, function (d) {
+        return d.decade;
+    }));
+    yScale.domain(d3.extent(data, function (d) {
+        return d.num_ids;
+    }));
+    svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg
+        .append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .attr("dx", ".71em")
+        .style("text-anchor", "beginning")
+        .text("number of titles");
+    var key_lines = svg
+        .selectAll(".term_key")
+        .data(key_data)
+        .enter()
+        .append("g")
+        .attr("class", "term_key");
+    key_lines
+        .append("path")
+        .attr("d", function (d) {
+            return line(d.datapoints);
+        })
+        .style("stroke", function (d) {
+            return color(d.term_key);
+        });
 
 });
+// Define responsive behavior
+function resize() {
+    var width =
+        parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+        height =
+            parseInt(d3.select("#chart").style("height")) -
+            margin.top -
+            margin.bottom;
+
+    // Update the range of the scale with new width/height
+    xScale.range([0, width]);
+    yScale.range([height, 0]);
+
+    // Update the axis and text with the new scale
+    svg
+        .select(".x.axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.select(".y.axis").call(yAxis);
+
+    // Force D3 to recalculate and update the line
+    svg.selectAll(".line").attr("d", function (d) {
+        return line(d.datapoints);
+    });
+
+    // Update the tick marks
+    xAxis.ticks(Math.max(width / 75, 2));
+    yAxis.ticks(Math.max(height / 50, 2));
+}
+
+// Call the resize function whenever a resize event occurs
+d3.select(window).on("resize", resize);
+
+// Call the resize function
+resize();
 console.log("end of script")
