69b99d03a774408d285c84eb8d8713da0e6f24db
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-07-08 17:20:54 -0400

    update

--- js/scripts_v1.js
+++ js/scripts_v1.js
@@ -1,18 +1,37 @@
 console.log("Beginning js script 8:37")
-var all_term_keys = []
+// Get list of all term keys from json
+var all_term_keys = Array(20)
 $.getJSON("https://raw.githubusercontent.com/Whole-Earth-Catalog/TermSearchResults/master/data/keys_by_decade.json", function (data) {
-    console.log("in json")
+    // console.log("in json")
+    var index = 0;
     $.each(data, function (i, d) {
         if ($.inArray(d.term_key, all_term_keys) === -1) {
-            all_term_keys.push(d.term_key)
+            all_term_keys[index] = d.term_key;
+            index++;
         }
     });
 });
 console.log(all_term_keys)
-var term_keys = ["justice"]
-console.log(term_keys)
+// Create form with label for each term key
+var form = document.getElementById("key_options");
+
+function add_option(term){
+    console.log("adding option")
+    var new_key_input = document.createElement("INPUT");
+    new_key_input.id = term + "Option"
+    new_key_input.type = "checkbox";
+    new_key_input.name = "dataset";
+    new_key_input.value = term;
+    var key_label = document.createElement("LABEL");
+    key_label.htmlFor = term + "Option"
+    key_label.innerHTML = term + " ";
+    form.insertBefore(document.createElement("BR"), null);
+    form.insertBefore(new_key_input, null);
+    form.insertBefore(key_label, new_key_input)
+}
+
 // Define margins
-var margin = { top: 20, right: 80, bottom: 30, left: 50 },
+var margin = { top: 50, right: 100, bottom: 100, left: 80 },
     width =
         parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
     height =
@@ -42,8 +61,10 @@ var svg = d3
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Define dataset
+
+// Draw graph from json
 d3.json("https://raw.githubusercontent.com/Whole-Earth-Catalog/TermSearchResults/master/data/keys_by_decade.json").then(function (data) {
+    var term_keys = all_term_keys
     console.log("reading from json...")
     // set color domain
     color.domain(term_keys)
@@ -52,17 +73,22 @@ d3.json("https://raw.githubusercontent.com/Whole-Earth-Catalog/TermSearchResults
         d.decade = Number(d.decade)
         d.num_ids = +d.num_ids
     });
-    var key_data = []
-    term_keys.forEach(function (key) {
-        datapoints = []
-        data.forEach(function (item) {
-            if (key == item.term_key && !isNaN(item.decade)) {
-                datapoints.push({"decade" : item.decade, "count": item.num_ids})
-            }
+    function clean_data(term_key_list) {
+        var key_data = []
+        term_key_list.forEach(function (key) {
+            datapoints = []
+            data.forEach(function (item) {
+                if (key == item.term_key && !isNaN(item.decade)) {
+                    datapoints.push({ "decade": item.decade, "count": item.num_ids })
+                }
+            })
+            key_data.push({ "term_key": key, "datapoints": datapoints })
         })
-        key_data.push({"term_key" : key, "datapoints" : datapoints})
-    })
-    console.log(key_data)
+        console.log(key_data)
+        return key_data
+    }
+    var key_data = clean_data(term_keys)
+    all_term_keys.forEach(add_option);
     // set domain of axes
     xScale.domain(d3.extent(data, function (d) {
         return d.decade;
@@ -102,42 +128,25 @@ d3.json("https://raw.githubusercontent.com/Whole-Earth-Catalog/TermSearchResults
             return color(d.term_key);
         })
         .attr("fill", "none");
-
+    function update(selected_term_keys) {
+        var new_key_data = clean_data(selected_term_keys);
+        key_lines
+            .data(new_key_data)
+            .enter()
+            .transition()
+            .attr("d", function (d) {
+                return line(d.datapoints);
+            })
+            .style("stroke", function (d) {
+                return color(d.term_key);
+            });
+    }
+    var new_list = ['violence', 'state']
+    document.getElementById("update_button").addEventListener("click", function () {
+        console.log("button pushed!")
+        update(new_list)
+    })
 });
-// Define responsive behavior
-function resize() {
-    var width =
-        parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
-        height =
-            parseInt(d3.select("#chart").style("height")) -
-            margin.top -
-            margin.bottom;
 
-    // Update the range of the scale with new width/height
-    xScale.range([0, width]);
-    yScale.range([height, 0]);
 
-    // Update the axis and text with the new scale
-    svg
-        .select(".x.axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.select(".y.axis").call(yAxis);
-
-    // Force D3 to recalculate and update the line
-    svg.selectAll(".line").attr("d", function (d) {
-        return line(d.datapoints);
-    });
-
-    // Update the tick marks
-    xAxis.ticks(Math.max(width / 75, 2));
-    yAxis.ticks(Math.max(height / 50, 2));
-}
-
-// Call the resize function whenever a resize event occurs
-d3.select(window).on("resize", resize);
-
-// Call the resize function
-resize();
 console.log("end of script")
