55f8b43522009d81b9483678856be05782e6aa1e
vascomadrid <vmadrid0426@gmail.com>
2020-07-08 15:34:20 -0700

    created clean_data json from finish python script

--- data/clean_data.py
+++ data/clean_data.py
@@ -7,8 +7,28 @@ Description:
 
 import json
 
+clean_data = {}
 with open("keys_by_decade.json") as f:
     data = json.load(f)
-    clean_data = []
-    for row in data:
-        print(row)
+    # print(data)
+    for elem in data:
+	# format attributes
+        term_key = elem['term_key']
+	try:
+            decade = int(elem['decade'])
+	except ValueError as e:
+            decade = 0
+	count = int(elem['num_ids'])
+	# only work with rows that have valid decade
+	if decade > 0:
+            datapoints = {'decade': decade, 'count': count}
+            # check if key is in dict, and update list
+            try:
+                clean_data[term_key].append(datapoints)
+	    except KeyError as e:
+		clean_data.update({term_key: [datapoints]}) 
+    #print(clean_data) 
+# write clean data to json file
+with open('clean_key_data.json', 'w') as f:
+    for key in clean_data:
+	f.write("{\"term_key\" : \"" + key + "\", \"datapoints\" : " + str(clean_data[key]) + "}")
