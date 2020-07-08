1c3d81b7bc0ec4d463374b19970f16ad48aa8a5a
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2020-07-08 18:58:10 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,40 @@ var margin = { top: 50, right: 100, bottom: 100, left: 80 },
         parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
     height =
         parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
+// Define scales
+var xScale = d3.scaleLinear().range([0, width]);
+var yScale = d3.scaleLinear().range([height, 0]);
+var color = d3.scaleOrdinal().range(d3.schemeCategory10);
+// Define axes
+var xAxis = d3.axisBottom().scale(xScale);
+var yAxis = d3.axisLeft().scale(yScale);
+// Define lines
+var line = d3
+    .line()
+    .curve(d3.curveMonotoneX)
+    .x(function (d) {
+        return xScale(d.decade)
+    })
+    .y(function (d) {
+        return yScale(d.count)
+    });
+// Define svg canvas
+var svg = d3
+    .select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// open data
+d3.json("https://raw.githubusercontent.com/Whole-Earth-Catalog/TermSearchResults/master/data/clean_key_data.json", function (data) {
+    all_term_keys = [];
+    // format data
+    data.forEach(function (row) {
+        all_term_keys.push(row.term_key)
+        row.forEach(function (d) {
+            d.count = +d.count;
+            d.decade = +d.decade;
+        });
+    });
+    console.log(data);
+});
